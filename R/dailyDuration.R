a6313ed6bb047be39f826b0d231abef196b0b743
Javier Baron <fjbaron@gmail.com>
2020-08-21 11:08:08 +0200

    robustnes changes

--- R/dailyDuration.R
+++ R/dailyDuration.R
@@ -9,13 +9,17 @@
 #'
 #' @examples
 dailyDuration=function(whenList,whatList,...){
+  if(is.null(whenList)) {
+    return(data.frame(day=today(),label="",what="",duration=0) %>% filter(FALSE))
+  }
   resultadoEsperado=crossing(whenList %>% select(day,label),what=names(whatList))
   duraciones=map2_df(whatList,names(whatList),~ {
     intervalIntersectv2(whenList,.x) %>% mutate(what=.y)
-  }) %>%
+  }) %>% 
     transmute(day=day,from=fromNew,to=toNew,what=what) %>% group_by(day,what) %>%
     summarise(duration=sum(as.integer(difftime(to,from,units="secs"))),.groups = 'drop') %>%
     ungroup() %>% arrange(what,day)
+  
   resultadoEsperado %>% left_join(duraciones, by = c("day", "what")) %>% mutate(duration=ifelse(is.na(duration),0,duration))
 }
 
--- R/intervalIntersectv2.R
+++ R/intervalIntersectv2.R
@@ -9,7 +9,9 @@
 #' @export
 intervalIntersectv2=function(interval1,interval2){
   if(is.null(interval1)) return(interval2 %>% mutate(fromNew=from,toNew=to,day=Sys.Date()) %>% filter(FALSE))
-  interval1=interval1 %>% mutate(.dummyColumn=1)
+  limits=c(first(interval2$from),last(interval2$to))
+  interval1=interval1 %>% mutate(.dummyColumn=1) 
+  if(!is.na(limits[1])) interval1=interval1 %>% filter(to>=limits[1],from<=limits[2]) 
 
   interval2=interval2 %>% mutate(.fromCut=from,.toCut=to) %>% mutate(.dummyColumn=1) %>% select(.fromCut,.toCut,.dummyColumn,everything(),-from,-to)
   resultado=interval1 %>% full_join(interval2,by=".dummyColumn") %>%
