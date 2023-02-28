5cf40e436e32f8e4bd3564429c37fdd08729e297
Francisco Javier Barón López <fjbaron@gmail.com>
2023-02-28 13:25:09 +0100

    Los intervalos what comienzan 5 segundos antes

--- R/criterio2Interval.R
+++ R/criterio2Interval.R
@@ -65,7 +65,7 @@ criterio2Interval=function(df,pctBouts=1,durBoutMin=dseconds(5),durEpoch=dsecond
 #Revisar
 data.frame(from=result_from) %>% 
   mutate(to=result_to[1:length(result_from)]) %>%
-  mutate(from=from-dseconds(2.5),to=to+dseconds(2.5)) %>% ##REVISAR SI ES ASI 27/02/2023
-  filter(from<to) %>% as_tibble()
+  mutate(from=from-dseconds(5)) %>% ##REVISAR SI ES ASI 27/02/2023
+  filter(from+dseconds(5)<to) %>% as_tibble()
 #data.frame(from=result_from,to=result_to) %>%as_tibble()
 }
