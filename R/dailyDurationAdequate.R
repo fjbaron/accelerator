901deb346d85d688e0e264f2761d7ac4020a6634
Francisco Javier Barón López <fjbaron@gmail.com>
2023-04-21 01:05:43 +0200

    Mejora de invalidDuration

--- R/dailyDurationAdequate.R
+++ R/dailyDurationAdequate.R
@@ -10,11 +10,13 @@
 #' @export
 #'
 #' @examples
-dailyDurationAdequate=function(whenList,whatList,durMin=NA,durMax=NA,...){
+dailyDurationAdequate=function(whenList,whatList,durMin=NA,durMax=NA,durRel=NA,...){
   if(!is.na(durMin)) durMin = as.numeric(duration(durMin,units="secs"))
   if(!is.na(durMax)) durMax = as.numeric(duration(durMax,units="secs"))
-  
+
   dailyDuration(whenList,whatList) %>% 
-    mutate(value= (is.na(durMin) | (durMin<= duration)) & (is.na(durMax) | (durMax>= duration))) %>%
+    mutate(value= (is.na(durMin) | (durMin<= duration)) &
+                  (is.na(durMax) | (durMax>= duration))
+           ) %>%
     select(day,what,value) 
 }
--- R/invalidDuration.R
+++ R/invalidDuration.R
@@ -5,6 +5,7 @@
 #' @param whenCond 
 #' @param minDur 
 #' @param maxDur 
+#' @param notIn 
 #' @param whenInvalid 
 #' @param reason 
 #'
@@ -12,14 +13,19 @@
 #' @export
 #'
 #' @examples
-invalidDuration=function(summary,whatCond,whenCond,minDur=NA,maxDur=NA,whenInvalid=NA_character_,reason="unexplained"){
+invalidDuration=function(summary,whatCond,whenCond,minDur=NA,maxDur=NA,notIn=NA,whenInvalid=NA_character_,reason="unexplained"){
   if(is.na(whenInvalid)) whenInvalid=whenCond
   asDurationMin="Duration" %in% class(minDur) 
   asDurationMax="Duration" %in% class(maxDur)
+  asDurationNotIn="Duration" %in% class(notIn)
+  
   summary %>% filter(when==whenCond,what==whatCond) %>%
-    mutate(loLim=ifelse(asDurationMin,minDur, as.duration(minDur*durationRef)),
-           hiLim=ifelse(asDurationMax,maxDur, as.duration(maxDur*durationRef))) %>% 
-    filter((!is.na(minDur) & as.duration(duration)<as.duration(loLim))|
-             (!is.na(maxDur) & as.duration(duration)>as.duration(hiLim)) ) %>%
+    mutate(loLim   =ifelse(asDurationMin,minDur, as.duration(minDur*durationRef)),
+           hiLim   =ifelse(asDurationMax,maxDur, as.duration(maxDur*durationRef))
+          )%>% 
+    filter(  (!is.na(minDur) & as.duration(duration)<as.duration(loLim))|
+             (!is.na(maxDur) & as.duration(duration)>as.duration(hiLim))|
+           (!is.na(notIn[1]) & (! as.duration(duration)  %in% (as.duration(notIn))))
+               ) %>%
     select(day) %>% mutate(when=whenInvalid,reason=reason)
 }
