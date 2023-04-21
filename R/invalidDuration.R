3e749790bf18f28a2e20b31c7ff1a78f94df5115
Francisco Javier Barón López <fjbaron@gmail.com>
2023-04-21 16:06:22 +0200

    more general invalidDuration

--- R/invalidDuration.R
+++ R/invalidDuration.R
@@ -13,19 +13,27 @@
 #' @export
 #'
 #' @examples
-invalidDuration=function(summary,whatCond,whenCond,minDur=NA,maxDur=NA,notIn=NA,whenInvalid=NA_character_,reason="unexplained"){
+invalidDuration=function(summary,whatCond,whenCond, 
+                         minDur=NA,
+                         maxDur=NA,
+                         notIn=NA,
+                         minRelative=NA,
+                         whenInvalid=NA_character_,
+                         reason="unexplained"){
   if(is.na(whenInvalid)) whenInvalid=whenCond
   asDurationMin="Duration" %in% class(minDur) 
   asDurationMax="Duration" %in% class(maxDur)
   asDurationNotIn="Duration" %in% class(notIn)
   
+
   summary %>% filter(when==whenCond,what==whatCond) %>%
     mutate(loLim   =ifelse(asDurationMin,minDur, as.duration(minDur*durationRef)),
-           hiLim   =ifelse(asDurationMax,maxDur, as.duration(maxDur*durationRef))
+           hiLim   =ifelse(asDurationMax,maxDur, as.duration(maxDur*durationRef)),
           )%>% 
     filter(  (!is.na(minDur) & as.duration(duration)<as.duration(loLim))|
              (!is.na(maxDur) & as.duration(duration)>as.duration(hiLim))|
-           (!is.na(notIn[1]) & (! as.duration(duration)  %in% (as.duration(notIn))))
+           (!is.na(notIn[1]) & (! as.duration(duration)  %in% (as.duration(notIn)))) |
+             (!is.na(minRelative) & duration< (durationRef*minRelative))
                ) %>%
     select(day) %>% mutate(when=whenInvalid,reason=reason)
 }
