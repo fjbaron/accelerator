71e2c238724720713d4ff51b415bd3a51086932d
Francisco Javier Barón López <fjbaron@gmail.com>
2023-04-22 14:28:44 +0200

    Protección ante when NULL

--- R/dailyDurationAdequate.R
+++ R/dailyDurationAdequate.R
@@ -15,6 +15,12 @@
 dailyDurationAdequate=function(whenList,whatList,durMin=NA,durMax=NA,durRelMin=NA,durRelMax=NA,...){
   if(!is.na(durMin)) durMin = as.numeric(duration(durMin,units="secs"))
   if(!is.na(durMax)) durMax = as.numeric(duration(durMax,units="secs"))
+  
+  #Proteccion antes when NULOS
+  if(!is.data.frame(whenList)){
+    whenList=data.frame(from=now(),to=now(),day=today(),label="VOID") %>% slice(0)
+  }
+  
   #Para usar duraciones relativas
   whenList=whenList %>% 
     mutate(durRef=as.numeric(difftime(to,from,units = "secs"))) %>%
